8017efb99ccfe76c3bd7cf9c25d146a442a6164b
Alex Gose <alexgose@users.noreply.github.com>
2023-04-17 14:22:18 -0400

    Refactor code with one call to pandas.get_dummies

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import numpy as np
 
-
 if __name__ == '__main__':
     df = pd.read_csv("ecommerce_data.csv")
-    time_of_day = df['time_of_day'].apply(lambda x: 'time_of_day_' + str(x))
-    one_hot = pd.get_dummies(time_of_day)
-    df = df.drop('time_of_day', axis=1)
-    df = pd.concat((df,one_hot))
+    df = pd.get_dummies(df, columns=['time_of_day'])  ## one-hot encode
     print(df)
     print(df.columns)
