9f6d416f6e9eff743084aa683d5f6f831d6ab671
Alex Gose <alexgose@users.noreply.github.com>
2023-05-10 17:42:35 -0400

    Change file name and move viz code to function

--- lda_model.py
+++ lda_model.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
+
+# Tests for logistic_regression.py using a linear discriminative analysis model
 import numpy as np
 import pandas as pd
 from logistic_regression import evaluate, mean_cross_entropy
 import matplotlib.pyplot as plt
+
+
+def visualize_lda(X, y):
+    fig,ax = plt.subplots()
+    ax.scatter(X[:,0], X[:,1], c=y.astype(np.int64).tolist(), s=100, alpha=0.5)
+    line_x = np.linspace(-5,5,100)
+    line_y = - line_x
+    ax.plot(line_x,line_y)
+    plt.show()
+
 
 if __name__ == '__main__':
     # generate fake data in accordance with LDA model
@@ -23,9 +35,4 @@ if __name__ == '__main__':
     outputs = evaluate(X, np.array([0, 4, 4]))
     print(mean_cross_entropy(outputs, y))
 
-    fig,ax = plt.subplots()
-    ax.scatter(X[:,0], X[:,1], c=y.astype(np.int64).tolist(), s=100, alpha=0.5)
-    line_x = np.linspace(-5,5,100)
-    line_y = - line_x
-    ax.plot(line_x,line_y)
-    plt.show()
+    visualize_lda(X, y)
