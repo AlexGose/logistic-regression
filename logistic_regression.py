6b99a5fe37884819bfaa8420f35f3c3b2b18f077
Alex Gose <alexgose@users.noreply.github.com>
2023-05-14 16:37:15 -0400

    Extend gradient descent code and return costs
    This extends the capabilities of the batch_gradient_descent
    function, returning the costs at each iteration and
    reporting final accuracy.
    
    This also optionally does the following:
    - takes test data as input and returns test costs
      at each iteration
    - runs in "verbose" mode
    - initializes the weights to random values

--- logistic_regression.py
+++ logistic_regression.py
@@ -15,7 +15,7 @@ def evaluate(inputs, weights):
     weights - F + 1 1D Numpy array representing the model weights.
               weights[0] is the bias term.  weights[i] corresponds to
               the ith feature.
-    
+
     Here, N represents the number of input data points and F is the
     number of input features.
     """
@@ -60,20 +60,54 @@ def gradient(inputs, targets, weights):
             * add_bias_feature(inputs)).sum(axis=0)
 
 
-def batch_gradient_ascent(inputs, targets, weights, lr=0.01, verbose=False,
-                          max_iters=1000, step_size=20):
+def batch_gradient_ascent(train_inputs, train_targets, initial_weights=None,
+                          lr=0.01, verbose=False, max_iters=1000,
+                          step_size=20, test_inputs=None, test_targets=None):
     """
     Full batch gradient ascent to maximize likelihood
 
-    Returns the weights at the last iteration
+    Returns the weights at the last iteration, train costs, and test costs
     """
+    test_data_exists = False
+    if np.any(test_inputs) and np.any(test_targets):
+        test_data_exists = True
+    if not np.any(initial_weights):
+        weights = np.random.randn(train_inputs.shape[1] + 1)
+    else:
+        weights = initial_weights
+
+    train_costs = []
+    test_costs = []
     for it in range(max_iters):
-        weights = weights + lr * gradient(inputs, targets, weights)
+
+        weights = weights + lr * gradient(train_inputs, train_targets, weights)
+
+        outputs = evaluate(train_inputs, weights)
+        train_cost = mean_cross_entropy(outputs, train_targets)
+        train_costs.append(train_cost)
+
+        if test_data_exists:
+            test_outputs = evaluate(test_inputs, weights)
+            test_cost = mean_cross_entropy(test_outputs, test_targets)
+            test_costs.append(test_cost)
+
         if verbose and it % step_size == step_size - 1:
-            outputs = evaluate(inputs, weights)
-            print(f"{it+1}/{max_iters}: "
-                  +f"{mean_cross_entropy(outputs, targets)}")
-    return weights
+            print(f"{it+1}/{max_iters}: train cost = {train_cost}")
+            if test_data_exists:
+                print(f"    test cost = {test_cost}")
+
+    if verbose:
+        train_predictions = predict(train_inputs, weights)
+        final_train_accuracy = classification_rate(train_predictions,
+                                                   train_targets)
+        print(f"Final train accuracy = {final_train_accuracy}")
+        if test_data_exists:
+            test_predictions = predict(test_inputs, weights)
+            final_test_accuracy = classification_rate(test_predictions,
+                                                      test_targets)
+            print(f"Final test accuracy = {final_test_accuracy}")
+
+    return weights, train_costs, test_costs
 
 
 if __name__ == '__main__':
@@ -82,19 +116,18 @@ if __name__ == '__main__':
     """
     print(sigmoid(0))
 
-    # generate random input data (20 data points with 2 features)
-    X = np.random.randn(20,2)
+    X = np.random.randn(20,2) # generate random input data
     w = np.random.randn(3)
 
     print(X.shape)
     print(evaluate(X,w))
     print(predict(X,w))
 
-    print(cross_entropy_loss(.9,1))
-    print(cross_entropy_loss(.1,0))
-    print(mean_cross_entropy(np.array([.9,.1]),np.array([1,0])))
+    print(cross_entropy_loss(.9, 1))
+    print(cross_entropy_loss(.1, 0))
+    print(mean_cross_entropy(np.array([.9, .1]), np.array([1, 0])))
 
-    y = np.random.choice([0,1], size=20)  # random labels
+    y = np.random.choice([0, 1], size=20)  # random labels
     print(gradient(X, y, w))
 
-    print(batch_gradient_ascent(X, y, w, lr=0.01, verbose=True, max_iters=100))
+    print(batch_gradient_ascent(X, y, lr=0.01, verbose=True, max_iters=100))
