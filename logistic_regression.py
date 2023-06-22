bf09d12212899d7465130436b6c193fc773e5d9c
Alex Gose <alexgose@users.noreply.github.com>
2023-06-22 13:48:00 -0400

    Add code for facial expression analysis
    Code in logistic_regression.py was also modified for
    better numerical stability and runtime performance

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,22 +1,14 @@
 import numpy as np
-from scipy.special import logsumexp
 
 
 def sigmoid(z):
-    return np.vectorize(_sigmoid)(z)
+    ans = np.zeros(z.shape)
+    ans[z<0] = np.exp(z[z<0]) / (np.exp(z[z<0]) + 1)
+    ans[z>=0] = 1 / (1 + np.exp(- z[z>=0]))
+    return ans
 
 
-def _sigmoid(z):
-    """
-    Numerically stable helper function to avoid overflow
-    """
-    if z >= 0:
-        return 1 / (1 + np.exp(- z))
-    else:
-        return np.exp(z) / (np.exp(z) + 1)
-
-
-def evaluate(inputs, weights):
+def evaluate(inputs, weights, has_bias_column=False):
     """
     Evaluate a logistic regression model at the input data points `inputs`
     using `weights`.  Return the probability that each data point
@@ -30,36 +22,48 @@ def evaluate(inputs, weights):
     Here, N represents the number of input data points and F is the
     number of input features.
     """
-    return sigmoid( add_bias_feature(inputs) @ weights )
+    if has_bias_column:
+        return sigmoid( inputs @ weights )
+    else:
+        return sigmoid( add_bias_feature(inputs) @ weights )
 
 
-def predict(inputs, weights):
-    return np.round(evaluate(inputs, weights))
+def predict(inputs, weights, has_bias_column=False):
+    return np.round(evaluate(inputs, weights, has_bias_column=has_bias_column))
 
 
 def classification_rate(predictions, targets):
     return (predictions == targets).mean()
 
 
-def cross_entropy_loss(feature_weight_product, target):
+def cross_entropy_loss(feature_weight_products, targets):
     """
     Numerically stable loss calculation
-
-    Uses log-sum-exp trick
     """
-    lse = logsumexp([0, - feature_weight_product])
-    return lse if target==1 else feature_weight_product + lse
+    lses = np.zeros(len(feature_weight_products))
+    lses[feature_weight_products <= 0] = np.log( 1 + np.exp(
+                    - feature_weight_products[feature_weight_products <= 0]))
+    lses[feature_weight_products > 0] = \
+            - feature_weight_products[feature_weight_products > 0] + np.log(1
+             + np.exp(feature_weight_products[feature_weight_products > 0]))
+    ans = np.zeros(len(feature_weight_products))
+    ans[targets==1] = lses[targets==1]
+    ans[targets==0] = feature_weight_products[targets==0] + lses[targets==0]
+    return ans
 
 
 def mean_cross_entropy(inputs, targets, weights, reg=False, reg_norm=1,
-                       reg_param=0.01):
+                       reg_param=0.01, has_bias_column=False):
     if reg:  # with regularization
         regularization = reg_param * np.linalg.norm(weights, reg_norm)
     else:
         regularization = 0
-    feature_weight_products = np.dot(add_bias_feature(inputs) , weights)
-    return np.vectorize(cross_entropy_loss)(feature_weight_products,
-                                            targets).mean() + regularization
+    if has_bias_column:
+        feature_weight_products = np.dot(inputs , weights)
+    else:
+        feature_weight_products = np.dot(add_bias_feature(inputs) , weights)
+    return cross_entropy_loss(feature_weight_products, targets).mean() \
+                                                        + regularization
 
 
 def add_bias_feature(inputs):
@@ -79,7 +83,7 @@ def add_interaction_feature(inputs, col1, col2):
 
 
 def gradient(inputs, targets, weights, l2=False, l2_param=0.01,
-             l1=False, l1_param=0.01):
+             l1=False, l1_param=0.01, has_bias_column=False):
     """
     Returns the gradient of the log likelihood w.r.t. the weights
 
@@ -93,12 +97,18 @@ def gradient(inputs, targets, weights, l2=False, l2_param=0.01,
     l1 - (default False) will include L1 regularization if True
     l1_param - (default 0.01) the regularization parameter for L1
                 regularization 
+    has_bias_column - (default False) equals true if `inputs` already has a
+                      column of ones for the bias term, False otherwise
 
     Here, N represents the number of input data points and F is the
     number of input features.
     """
-    output = ((targets - evaluate(inputs, weights)).reshape((-1,1))
-            * add_bias_feature(inputs)).sum(axis=0)
+    if not has_bias_column:
+        output = ((targets - evaluate(inputs, weights)).reshape((-1,1))
+                   * add_bias_feature(inputs)).sum(axis=0)
+    else:
+        output = ((targets - evaluate(inputs, weights, has_bias_column=True))
+                   * inputs.T).T.sum(axis=0)
     if l2:
         output -= l2_param * weights
     if l1:
@@ -109,18 +119,25 @@ def gradient(inputs, targets, weights, l2=False, l2_param=0.01,
 def batch_gradient_ascent(train_inputs, train_targets, initial_weights=None,
                           lr=0.01, verbose=False, max_iters=1000,
                           step_size=20, test_inputs=None, test_targets=None,
-                          l2=False, l2_param=0.01, l1=False, l1_param=0.01):
+                          l2=False, l2_param=0.01, l1=False, l1_param=0.01,
+                          has_bias_column=False):
     """
     Full batch gradient ascent to maximize log likelihood
 
     Returns the weights at the last iteration, train costs, and test costs
     """
+    if not has_bias_column:
+        train_inputs = add_bias_feature(train_inputs)
+
     test_data_exists = True
     if test_inputs is None or test_targets is None:
         test_data_exists = False
+    else:
+        if not has_bias_column:
+            test_inputs = add_bias_feature(test_inputs)
 
     if initial_weights is None:
-        num_weights = train_inputs.shape[1] + 1
+        num_weights = train_inputs.shape[1]
         weights = np.random.randn(num_weights) / np.sqrt(num_weights)
     else:
         weights = initial_weights.astype(np.float64)
@@ -128,17 +145,17 @@ def batch_gradient_ascent(train_inputs, train_targets, initial_weights=None,
     train_costs = []
     test_costs = []
     for it in range(max_iters):
-
         weights += lr * gradient(train_inputs, train_targets, weights, 
-                          l2=l2, l2_param=l2_param, l1=l1, l1_param=l1_param)
+                          l2=l2, l2_param=l2_param, l1=l1, l1_param=l1_param,
+                          has_bias_column=True)
 
-        outputs = evaluate(train_inputs, weights)
-        train_cost = mean_cross_entropy(train_inputs, train_targets, weights)
+        train_cost = mean_cross_entropy(train_inputs, train_targets, weights,
+                                    has_bias_column=True)
         train_costs.append(train_cost)
 
         if test_data_exists:
-            test_outputs = evaluate(test_inputs, weights)
-            test_cost = mean_cross_entropy(test_inputs, test_targets, weights)
+            test_cost = mean_cross_entropy(test_inputs, test_targets, weights,
+                                           has_bias_column=True)
             test_costs.append(test_cost)
 
         if verbose and it % step_size == step_size - 1:
@@ -147,12 +164,14 @@ def batch_gradient_ascent(train_inputs, train_targets, initial_weights=None,
                 print(f"    test cost = {test_cost}")
 
     if verbose:
-        train_predictions = predict(train_inputs, weights)
+        train_predictions = predict(train_inputs, weights,
+                                    has_bias_column=True)
         final_train_accuracy = classification_rate(train_predictions,
                                                    train_targets)
         print(f"Final train accuracy = {final_train_accuracy}")
         if test_data_exists:
-            test_predictions = predict(test_inputs, weights)
+            test_predictions = predict(test_inputs, weights,
+                                       has_bias_column=True)
             final_test_accuracy = classification_rate(test_predictions,
                                                       test_targets)
             print(f"Final test accuracy = {final_test_accuracy}")
